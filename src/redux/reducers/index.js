634ffc21409537639f62bb28ab307d287f78164e
Douglas Lira <douglas.lira.web@gmail.com>
2020-09-06 01:36:01 +0100

    Fix redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,16 +1,18 @@
-import { ADD_TASK } from "../constants";
+import { ADD_TASK, LIST_TASK } from "../constants";
 
 const initialState = {
     tasks: [{id: 1, title: 'Task 01', description: 'Description task 01', status: 1, date: new Date()}]
 };
 
 function rootReducer(state = initialState, action) {
-    if (action.type === ADD_TASK) {
-      return Object.assign({}, state, {
-        tasks: state.tasks.concat(action.newObj)
-      });
-    }
-    return state;
-  }
+	if (action.type === ADD_TASK) {
+		return Object.assign({}, state, {
+			tasks: state.tasks.concat(action.newObj)
+		});
+	} else if(action.type === LIST_TASK) {
+		return state.tasks;
+	}
+	return state;
+}
 
 export default rootReducer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,82 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,  
-            exclude: /node_modules/,  
-            use: {  
-                loader: 'babel-loader',  
-                options: {  
-                    cacheDirectory: true  
-                }  
-            }  
-        },{
-            test: /\.html$/,
-            use: [{
-                loader: "html-loader",
-                options: { minimize: true }
-            }]
-        },{
-            test: /\.scss$/,
-            use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader", "sass-loader"]
-        },{
-            test: /\.(png|jpg)$/,
-            use: {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "images/"
-              }
-            }
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'React + Webpack :: Douglas Lira',
-            template: __dirname + '/template/index.html',
-            meta: {
-                viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
-            },
-            hash: true
-        }),
-        new MiniCssExtractPlugin({
-          filename: "css/[name].css"
-        })
+const PATH_SOURCE = path.join(__dirname, './src');
+const PATH_DIST = path.join(__dirname, './dist');
+
+module.exports = env => {
+
+	const environment = env.environment;
+  const isProduction = environment === 'production';
+  const isDevelopment = environment === 'development';
+
+	return {
+		mode: environment,
+		entry: [
+      path.join(PATH_SOURCE, './index.js'),
     ],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:10010'
-        })
-    }
+		output: {
+			path: __dirname + '/dist',
+			filename: 'js/[name].[hash].js'
+		},
+		module: {
+			rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true
+					}
+				}
+			},{
+				test: /\.html$/,
+				use: [{
+					loader: "html-loader",
+					options: { minimize: true }
+				}]
+			},{
+				test: /\.scss$/,
+				use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader", "sass-loader"]
+			},{
+				test: /\.(png|jpg)$/,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "[name].[ext]",
+						outputPath: "images/"
+					}
+				}
+			}]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: 'React + Webpack :: Douglas Lira',
+				template: __dirname + '/template/index.html',
+				meta: {
+					viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+				},
+				hash: true
+			}),
+			new MiniCssExtractPlugin({
+				filename: "css/[name].[hash].css"
+			}),
+			new CleanWebpackPlugin(),
+		],
+		devServer: {
+			contentBase: PATH_DIST,
+			historyApiFallback: true,
+			overlay: {
+        errors: true,
+        warnings: true,
+      },
+		},
+		externals: {
+			config: JSON.stringify({
+				apiUrl: 'http://localhost:10010'
+			})
+		}
+	}
 };
